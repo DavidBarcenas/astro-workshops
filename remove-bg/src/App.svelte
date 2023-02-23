65628e6210988e65174b18b1326235e4064e1423
David Barcenas <dbarcenas.dev@gmail.com>
2023-02-22 21:22:37 -0600

    remove-bg: create layout

--- remove-bg/src/App.svelte
+++ remove-bg/src/App.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
+  import CloudinaryLogo from "./cloudinary-logo.svelte";
 </script>
 
-<main>
-  <h1>Vite + Svelte</h1>
-</main>
+<div class="max-w-xl m-auto grid w-full h-screen">
+  <header class="flex justify-center py-10">
+    <h1 class="text-3xl font-bold text-blue-900 tracking-tighter">
+      remove<span class="text-blue-600">bg</span>
+    </h1>
+  </header>
+
+  <main>
+    <h2>content</h2>
+  </main>
+
+  <footer class="flex justify-center items-center gap-x-2 font-semibold pt-10">
+    Hecho con <a href="https://cloudinary" target="_blank" rel="noreferrer">
+      <CloudinaryLogo />
+    </a>
+  </footer>
+</div>
