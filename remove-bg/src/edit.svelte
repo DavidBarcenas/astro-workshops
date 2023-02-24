f7e2f946bb15963ddb058a23aff9ee4b94365dfa
David Barcenas <dbarcenas.dev@gmail.com>
2023-02-23 21:51:59 -0600

    remove-bg: add web component for images

---
+++ remove-bg/src/edit.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import "two-up-element";
+  import { modifiedImage, originalImage } from "./store";
+</script>
+
+<two-up>
+  <img src={$originalImage} alt="Original image" />
+  <img src={$modifiedImage} alt="Image without bg" />
+</two-up>
+
+<a download href="/"> Download image without bg </a>
