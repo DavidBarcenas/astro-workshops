3f3a6fa1a573afefb24b815ea596bed4b592230d
David Barcenas <dbarcenas.dev@gmail.com>
2023-02-25 16:58:00 -0600

    remove-bg: validate image without bg

--- remove-bg/src/edit.svelte
+++ remove-bg/src/edit.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
   import "two-up-element";
   import { modifiedImage, originalImage } from "./store";
+
+  let processingImage = true;
+  const loadImage = (url: string) => {
+    const img = new Image();
+    img.src = $modifiedImage;
+    img.onload = () => {
+      processingImage = false;
+    };
+    img.onerror = () => {
+      setTimeout(() => loadImage(url), 1000);
+    };
+  };
+
+  $: {
+    loadImage($modifiedImage);
+  }
 </script>
 
 <two-up>
   <img src={$originalImage} alt="Original image" />
-  <img src={$modifiedImage} alt="Image without bg" />
+  {#if processingImage}
+    <div class="flex flex-col justify-center items-center">
+      <p class="text-center mt-4">Procesando imagen...</p>
+    </div>
+  {:else}
+    <img src={$modifiedImage} alt="Image without bg" />
+  {/if}
 </two-up>
 
 <a download href="/"> Download image without bg </a>
