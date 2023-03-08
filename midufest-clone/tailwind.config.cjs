ca2613a002856a3317b67af3178caa8752a1eab0
David Barcenas <dbarcenas.dev@gmail.com>
2023-03-07 19:41:50 -0600

    midufest: implement animations

--- midufest-clone/tailwind.config.cjs
+++ midufest-clone/tailwind.config.cjs
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const { withAnimations } = require("animated-tailwindcss");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
-module.exports = {
+module.exports = withAnimations({
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#FFD200",
+      },
       sans: {
         fontFamily: ["Montserrat", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
-};
+});
