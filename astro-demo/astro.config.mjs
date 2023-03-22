adf71f1994c30e4fcbea0e9e5734c6c484489fc0
David Barcenas <dbarcenas.dev@gmail.com>
2023-03-21 22:44:36 -0600

    astro-demo: add SEO, RSS and sitemap

--- astro-demo/astro.config.mjs
+++ astro-demo/astro.config.mjs
@@ -3,8 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue()],
+  site: 'https://stro.demo',
+  integrations: [tailwind(), mdx(), react(), vue(), sitemap()],
 });
