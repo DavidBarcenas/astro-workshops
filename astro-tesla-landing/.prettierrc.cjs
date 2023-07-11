499ded5baa3feb6ed8708148dfe09f667b15269e
David Barcenas <dbarcenas.dev@gmail.com>
2023-07-10 21:25:06 -0600

    chore(tesla-landing): setup prettier

---
+++ astro-tesla-landing/.prettierrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+      printWidth: 100,
+      semi: false,
+      singleQuote: true,
+      jsxSingleQuote: true,
+      tabWidth: 2,
+      trailingComma: "none",
+      useTabs: false,
+      endOfLine: "lf",
+      bracketSpacing: true,
+      quoteProps: "as-needed",
+      arrowParens: "always",
+      htmlWhitespaceSensitivity: "css",
+      jsxBracketSameLine: false,
+    },
+  ],
+};
