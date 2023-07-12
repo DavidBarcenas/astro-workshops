8bd47e701aed6acfe5f4907dadce62fcbd8bd905
David Barcenas <dbarcenas.dev@gmail.com>
2023-07-12 15:49:42 -0600

    fix(tesla-landing): prettier plugins version

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,24 +1,25 @@
 module.exports = {
-    plugins: [require.resolve("prettier-plugin-astro")],
-    overrides: [
-        {
-            files: "*.astro",
-            options: {
-                parser: "astro",
-            },
-            printWidth: 100,
-            semi: false,
-            singleQuote: true,
-            jsxSingleQuote: true,
-            tabWidth: 2,
-            trailingComma: "none",
-            useTabs: false,
-            endOfLine: "lf",
-            bracketSpacing: true,
-            quoteProps: "as-needed",
-            arrowParens: "always",
-            htmlWhitespaceSensitivity: "css",
-            jsxBracketSameLine: false,
-        },
-    ],
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+      pluginSearchDirs: ["."],
+      printWidth: 100,
+      semi: false,
+      singleQuote: true,
+      jsxSingleQuote: true,
+      tabWidth: 2,
+      trailingComma: "none",
+      useTabs: false,
+      endOfLine: "lf",
+      bracketSpacing: true,
+      quoteProps: "as-needed",
+      arrowParens: "always",
+      htmlWhitespaceSensitivity: "css",
+      jsxBracketSameLine: false,
+    },
+  ],
 };
