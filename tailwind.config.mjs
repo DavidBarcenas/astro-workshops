bfebfd1e2727b1ef19b3fd0f0bef5a035f5371f7
David Barcenas <dbarcenas.dev@gmail.com>
2024-01-01 21:29:15 -0600

    esland: add video with animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
+import animations from "tailwindcss-animated";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    animations,
+    ({ addComponents }) => {
+      addComponents({
+        ".clip-path-v": {
+          clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)",
+        },
+      });
+    },
+  ],
+
 };
